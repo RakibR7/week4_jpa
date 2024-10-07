db21778f54d357fee281ab55955244a1465e678e
Rakib Rana <G00408644@atu.ie>
2024-10-07 13:25:36 +0100

    V2

--- src/main/java/ie/atu/week4/jpa/ProductService.java
+++ src/main/java/ie/atu/week4/jpa/ProductService.java
@@ -1,13 +1,14 @@
 package ie.atu.week4.jpa;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ProductService {
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
